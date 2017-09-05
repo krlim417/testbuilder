516d677b0bc56abb5c47d194e5e599b7df35aff8
krlim417 <krlim417@gmail.com>
2017-09-05 11:35:15 -0700

    Fixes Diner's Club test bugs

--- detectNetwork.test.js
+++ detectNetwork.test.js
@@ -37,22 +37,20 @@ describe('Introduction to Mocha Tests - READ ME FIRST', function() {
     }
   });
 });
+
 describe('Diner\'s Club', function() {
   // Be careful, tests can have bugs too...
 
   it('has a prefix of 38 and a length of 14', function() {
-    throw new Error('Delete me!');
-
     if (detectNetwork('38345678901234') !== 'Diner\'s Club') {
       throw new Error('Test failed');
     }
   });
 
   it('has a prefix of 39 and a length of 14', function() {
-    if (detectNetwork('3934567890123') !== 'Diner\'s Club') {
+    if (detectNetwork('39345678901234') !== 'Diner\'s Club') {
       throw new Error('Test failed');
     }
-
   });
 });
 
